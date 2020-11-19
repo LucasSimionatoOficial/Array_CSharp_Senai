9e512b2c0431b68df5eabc244debaabaee6d332f
Lucas <lucasimionato1910@hotmail.com>
2020-11-19 18:17:30 -0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] compras = {"maçã", "batata", "cenoura"};
+            double[] preco = {1.10, 2.30, 4.50};
+            int[] idades = {16, 17, 17};
+
+            string[] nomesAlunos = new string[5];
+
+            // Console.WriteLine(compras[2]);
+
+            // string item = Console.ReadLine();
+
+            // Console.WriteLine("Trocar batata pelo quê?");
+            // compras[1] = Console.ReadLine();
+
+            // Console.WriteLine(compras[1]);
+
+            for (var i = 0; i < compras.Length; i++)
+            {
+                Console.WriteLine(compras[i] + ": R$" + preco[i]);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Array_CSharp_Senai
